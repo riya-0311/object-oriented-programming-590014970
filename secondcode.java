20528b9be5dfe873ec7664796b22689c8b6cccef
riya-0311 <dr.riya8176@gmail.com>
2026-01-22 12:02:35 +0530

    feat: secondcode.java

---
+++ secondcode.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+class Main {
+    int n;
+    int sum = 0;
+
+    public static void main(String[] args) {
+        Main m = new Main();
+        Scanner s = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        m.n = s.nextInt();
+
+       
+        m.sum = m.n;
+        System.out.println("Sum = " + m.sum);
+
+        s.close();
+    }
+}
+
+class Car {
+    int modelYear;
+    int mileage;
+
+    public static void main(String[] args) {
+        Car c1 = new Car();
+        Car c2 = new Car();
+
+        c1.modelYear = 2020;
+        c1.mileage = 15;
+
+        c2.modelYear = 2022;
+        c2.mileage = 18;
+
+        System.out.println("Car 1 Year: " + c1.modelYear + ", Mileage: " + c1.mileage);
+        System.out.println("Car 2 Year: " + c2.modelYear + ", Mileage: " + c2.mileage);
+    }
+}
