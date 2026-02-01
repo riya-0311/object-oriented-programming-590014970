264b89042c5faf804c5c80ffc47e2fc757c32d75
riya-0311 <dr.riya8176@gmail.com>
2026-02-01 12:42:27 +0530

    feat : array_sum_command_line.java

---
+++ array_sum_command_line.java
@@ -0,0 +1,17 @@
+public class array_sum_command_line {
+ 
+    public static void main(String[] args) {
+
+        int sum = 0;
+
+        for (int i = 0; i < args.length; i++) {
+            sum = sum + Integer.parseInt(args[i]);
+        }
+
+        System.out.println("Sum = " + sum);
+    }
+}
+  
+    
+
+
