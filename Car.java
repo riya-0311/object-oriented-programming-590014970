3ed0a6de733727ba06e7002a83dbe981ea0c5847
riya-0311 <dr.riya8176@gmail.com>
2026-02-01 14:06:39 +0530

    feat : parent.java

---
+++ Car.java
@@ -0,0 +1,12 @@
+public class Car {
+     Car() {
+        System.out.println("Constructor of Car class is called");
+    }
+}
+class MainClass {
+    public static void main(String[] args) {
+        Car c = new Car();   
+    }
+}
+    
+
