f063820c7ed40c56aac44ec1f4d5a56113f97866
riya-0311 <dr.riya8176@gmail.com>
2026-02-18 21:36:06 +0530

    feat : thread_class.java

---
+++ thread_class.java
@@ -0,0 +1,32 @@
+class t1 extends Thread{
+    public void run(){
+        for(int i = 0 ; i <= 5 ; i++){
+            System.out.println(i);
+        }
+    }
+}
+    class t2 extends Thread{
+    public void run(){
+        for(int i = 6 ; i <= 10 ; i++){
+            System.out.println(i);
+        }
+    }
+
+}
+public class thread_class {
+
+    public static void main(String[] args) {
+        t1 a = new t1();
+        t2 b = new t2();
+        a.start();
+        try{
+        a.join();
+    }catch (InterruptedException e) {
+            e.printStackTrace();
+
+    }
+        b.start();
+        
+    }
+    
+}
